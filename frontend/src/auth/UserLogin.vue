33e5322a04ef3a82083e29a3afb9873cef124a99
Nishkarsh Sharma <nishkarshsharma540@gmail.com>
2025-10-06 20:37:01 +0530

    Day3 bootcamp

---
+++ frontend/src/auth/UserLogin.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="loginUser">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <div v-if="errorMessage">{{ errorMessage }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async loginUser() {
+      try {
+        const response = await axios.post("http://127.0.0.1:5000/login", {
+          username: this.username,
+          password: this.password,
+        });
+        const { access_token, user_info } = response.data;
+        console.log(user_info);
+        localStorage.setItem("access_token", access_token);
+        localStorage.setItem("user", JSON.stringify(user_info));
+
+        if (user_info.role === "admin") {
+          this.$router.push("/admin_dashboard");
+        } else {
+          this.$router.push("/dashboard");
+        }
+      } catch (error) {
+        this.errorMessage = "Login failed. Please try again.";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SignupUser from '../auth/UserSignup.vue'
+import LoginUser from '../auth/UserLogin.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupUser
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginUser
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
